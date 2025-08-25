3a021eec4733a4b808ada64d9e304ab961d11b7d
amolpatil1955 <163282417+amolpatil1955@users.noreply.github.com>
2025-08-25 15:59:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import authRoutes from "./src/routes/auth.route.js";
 import userRoutes from "./src/routes/user.route.js";
 import chatRoutes from "./src/routes/chat.route.js";
 
-import { connectDB } from "./lib/db.js";
+import { connectDB } from "./src/lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
